218e508f977cf5b99bf53220b47b97cc9db2c941
ernolf <raphael.gradenwitz@googlemail.com>
2026-05-31 20:14:02 +0200

    refactor: derive dnsmasq config dir from CONFIG_DIR
    - dcm-cli: read DNSMASQ_D from /etc/default/dnsmasq (CONFIG_DIR) instead of hardcoding it
    - config.php: parse CONFIG_DIR in the same pass as the conf-file path
    
    Signed-off-by: ernolf <raphael.gradenwitz@googlemail.com>

--- www/inc/config.php
+++ www/inc/config.php
@@ -2,21 +2,25 @@
 // SPDX-FileCopyrightText: 2026 [ernolf] Raphael Gradenwitz
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-define('DNSMASQ_D',     '/etc/dnsmasq.d');
+// Derive paths from /etc/default/dnsmasq (single source of truth)
+$_dconf = '/etc/dnsmasq.conf.mine';
+$_ddir  = '/etc/dnsmasq.d';
+foreach (file('/etc/default/dnsmasq', FILE_IGNORE_NEW_LINES) ?: [] as $_l) {
+    if (str_starts_with(ltrim($_l), '#')) continue;
+    if (preg_match('/DNSMASQ_OPTS.*--conf-file=([^ "\']+)/', $_l, $_m)) {
+        $_dconf = $_m[1];
+    } elseif (preg_match('/^\s*CONFIG_DIR=([^,\s"\']+)/', $_l, $_m)) {
+        // strip the trailing ,.dpkg-* exclusion suffixes
+        $_ddir = $_m[1];
+    }
+}
+
+define('DNSMASQ_CONF',  $_dconf);
+define('DNSMASQ_D',     $_ddir);
 define('HOSTS_DIR',     DNSMASQ_D . '/hosts');
 define('UPSTREAM_CONF', DNSMASQ_D . '/upstream.conf');
 define('NODES_FILE',    '/etc/dcm/nodes');
 define('CLI',           '/usr/local/sbin/dcm-cli');
 define('LOG_FILE',      '/var/log/dnsmasq/dnsmasq.log');
 
-// Derive conf-file path from /etc/default/dnsmasq (single source of truth)
-$_dconf = '/etc/dnsmasq.conf.mine';
-foreach (file('/etc/default/dnsmasq', FILE_IGNORE_NEW_LINES) ?: [] as $_l) {
-    if (str_starts_with(ltrim($_l), '#')) continue;
-    if (preg_match('/DNSMASQ_OPTS.*--conf-file=([^ "\']+)/', $_l, $_m)) {
-        $_dconf = $_m[1];
-        break;
-    }
-}
-define('DNSMASQ_CONF', $_dconf);
-unset($_dconf, $_l, $_m);
+unset($_dconf, $_ddir, $_l, $_m);
